81dfb5fd0e1dc14278fdaee2253b8587468f30a2
Theo Ferguson <ferguson.theo@gmail.com>
2022-02-08 10:15:37 -0500

    error message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
+  // function handleChange(e) {
+  //   setText(e.target.value);
+  // }
+
+  function handleGetText() {
+    fetch('www.getrandomtext.com')
+      .then((r) => r.json())
+      .then((data) => {
+        setText("hello world")
+      })
+      .catch((error) => {
+        setText("hello world")
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <button onClick={handleGetText}>get text</button>
+        <input type="text" name="inputHere" value={text} onChange={(e) => setText(e.target.value)} />
+        {text.toUpperCase()}
+      </p>
     </div>
   );
 }
